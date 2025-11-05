84216c972e5570a879151f95114f75ebb51220e1
Kaczmarek076 <sebkac01@easv365.dk>
2025-11-05 13:08:02 +0100

    New calc (calculator for short)

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+
+
+    /**
+     * Hella coll method to add numbers
+     * @param a
+     * @param b
+     * @return
+     */
+    public static int sum (int a, int b){
+        return a+b;
+    }
+}
